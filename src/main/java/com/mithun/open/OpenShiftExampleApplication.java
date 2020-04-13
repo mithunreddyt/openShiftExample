8381feaa0b864c791eb029153a5ee4601a8ff9a3
Mithun Reddy Tunga <mithunreddy.tunga@eagles.oc.edu>
2020-04-13 00:24:07 -0500

    Initial commit

---
+++ src/main/java/com/mithun/open/OpenShiftExampleApplication.java
@@ -0,0 +1,13 @@
+package com.mithun.open;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenShiftExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenShiftExampleApplication.class, args);
+	}
+
+}
